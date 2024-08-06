60cf606ea4635564e3350f04a5215b72a88b9556
Sparsh <sparshkhanna1@gmail.com>
2024-08-06 17:18:00 +0530

    Added Django environ var

--- src/skhome/settings.py
+++ src/skhome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#=nftn0-py51q!il_!ix#_k+!na^y1l_yfc_$xs3j9yfxls5oq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas/prod.railway.app
