54b38bfee2e0585b94766dceea4ad5024e212283
Sparsh <sparshkhanna1@gmail.com>
2024-08-22 11:57:25 +0530

    Set Up Navba Links for auth

--- src/skhome/urls.py
+++ src/skhome/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.login_view),
     path('register/', auth_views.register_view),
-    path('hello-world/', home_view),
-    path('', home_view),
+    path('hello-world/', home_view), # index page -> root page
+    path('', home_view, name='home'),
     path('hello-world.html', home_view),
     path('accounts/', include('allauth.urls')), 
     path('about/', about_view),
