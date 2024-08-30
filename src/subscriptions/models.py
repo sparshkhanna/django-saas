ff5c4201ef6e649e363eebd195ba14cc81460cde
Sparsh <sparshkhanna1@gmail.com>
2024-08-30 12:04:17 +0530

    Subcription model added with syncing subcription groups and permission

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -11,6 +11,7 @@ SUBSCRIPTION_PERMISSIONS = [
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
     permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", "codename__in":[x[0] for x in SUBSCRIPTION_PERMISSIONS]}) # list comprehension technique
     class Meta:
