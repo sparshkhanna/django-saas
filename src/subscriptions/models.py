442c54e27cb89af32c45de0248c78a7736dad536
Sparsh <sparshkhanna1@gmail.com>
2024-09-03 15:28:04 +0530

    User subcription model added 1

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
-# Create your models here.
+
+from django.conf import settings
+User = settings.AUTH_USER_MODEL # "auth.User"
 
 SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advance Perm"), # subscriptions.advance
@@ -14,5 +16,16 @@ class Subscription(models.Model):
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
     permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", "codename__in":[x[0] for x in SUBSCRIPTION_PERMISSIONS]}) # list comprehension technique
+    
+    
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+        
+        
+        
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    
+    
