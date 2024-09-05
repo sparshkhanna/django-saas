c529d5fa4aa93353df8931028f362eae4c607bb7
Sparsh <sparshkhanna1@gmail.com>
2024-09-05 11:23:35 +0530

    Sync user subscription with user groups

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
-
+from django.db.models.signals import post_save
 from django.conf import settings
 User = settings.AUTH_USER_MODEL # "auth.User"
 
+ALLOW_CUSTOM_GROUPS = True
 SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advance Perm"), # subscriptions.advance
             ("pro", "Pro Perm"), # subcriptions.pro
@@ -19,7 +20,7 @@ class Subscription(models.Model):
     
     
     def __str__(self):
-        return self.name
+        return f"{self.name}"
     
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
@@ -31,4 +32,26 @@ class UserSubscription(models.Model):
     subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
     active = models.BooleanField(default=True)
     
-    
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    user.groups.set(groups) 
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups)
+    else:
+        subs_qs = Subscription.objects.filter(active=True).exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups) 
+        groups_ids = groups.values_list('id', flat=True) # [1, 2, 3]
+        current_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set 
+        final_groups_id = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_groups_id)
+        
+post_save.connect(user_sub_post_save, sender=UserSubscription)
