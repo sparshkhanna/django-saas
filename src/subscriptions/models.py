4990ef98fe8ca3c535c2c4f18624b1ecf0058293
Sparsh <sparshkhanna1@gmail.com>
2024-09-06 16:03:04 +0530

    Minor Bug Fixes

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_save
 from django.conf import settings
+
 User = settings.AUTH_USER_MODEL # "auth.User"
 
 ALLOW_CUSTOM_GROUPS = True
@@ -40,9 +41,8 @@ def user_sub_post_save(sender, instance, *args, **kwargs):
     if subscription_obj is not None:
         groups = subscription_obj.groups.all()
         groups_ids = groups.values_list('id', flat=True)
-    user.groups.set(groups) 
     if not ALLOW_CUSTOM_GROUPS:
-        user.groups.set(groups)
+        user.groups.set(groups_ids)
     else:
         subs_qs = Subscription.objects.filter(active=True)
         if subscription_obj is not None:
@@ -53,7 +53,7 @@ def user_sub_post_save(sender, instance, *args, **kwargs):
         current_groups = user.groups.all().values_list('id', flat=True)
         groups_ids_set = set(groups_ids)
         current_groups_set = set(current_groups) - subs_groups_set 
-        final_groups_id = list(groups_ids_set | current_groups_set)
-        user.groups.set(final_groups_id)
+        final_group_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
         
 post_save.connect(user_sub_post_save, sender=UserSubscription)
