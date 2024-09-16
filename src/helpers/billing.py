baf11a204d195bb1c32efc5cc53fb8fcbb59fcbf
Sparsh <sparshkhanna1@gmail.com>
2024-09-16 12:36:18 +0530

    Config stripe secret key

---
+++ src/helpers/billing.py
@@ -0,0 +1,15 @@
+import stripe
+
+from decouple import config
+
+DJANGO_DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="", cast=str)
+
+if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
+    raise ValueError("Invalid stripe key for prod")
+
+stripe.api_key = STRIPE_SECRET_KEY
+stripe.Customer.create(
+  name="Jenny Rosen",
+  email="jennyrosen@example.com",
+)
