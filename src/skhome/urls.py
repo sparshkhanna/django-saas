7e2525d4ef1adbaf16bfd9144109eaf0f332daf5
Sparsh <sparshkhanna1@gmail.com>
2024-10-09 16:32:36 +0530

    Ordered pricing configured and pricing view and template added

--- src/skhome/urls.py
+++ src/skhome/urls.py
@@ -2,16 +2,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
+from subscriptions import views as subscription_views
 from .views import (home_view, 
                     about_view, 
                     pw_protected_view,
                     user_only_view,
                     staff_only_view)
+
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login_view),
-    path('register/', auth_views.register_view),
+    # path('login/', auth_views.login_view),
+    # path('register/', auth_views.register_view),
+    path('pricing/', subscription_views.subscription_price_view, name='pricing'),
     path('hello-world/', home_view), # index page -> root page
     path('', home_view, name='home'),
     path('hello-world.html', home_view),
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -8,7 +8,7 @@ User = settings.AUTH_USER_MODEL # "auth.User"
 
 ALLOW_CUSTOM_GROUPS = True
 SUBSCRIPTION_PERMISSIONS = [
-            ("advanced", "Advance Perm"), # subscriptions.advance
+            ("advanced", "Advance Perm"), # subscriptions.advance 
             ("pro", "Pro Perm"), # subcriptions.pro
             ("basic", "Basic Perm"), # subscriptions.basic
             ("basic_ai", "Basic AI Perm") 
@@ -24,10 +24,17 @@ class Subscription(models.Model):
     permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", "codename__in":[x[0] for x in SUBSCRIPTION_PERMISSIONS]}) # list comprehension technique
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
     
+    order = models.IntegerField(default=-1, help_text='Ordering on Django pricing page')
+    featured = models.BooleanField(default=True, help_text='Featured on Django pricing page')
+    updated = models.DateTimeField(auto_now=True)
+    Timestamp = models.DateField(auto_now_add=True)
+        
+    
     def __str__(self):
         return f"{self.name}"
     
     class Meta:
+        ordering = ['order', 'featured', '-updated']
         permissions = SUBSCRIPTION_PERMISSIONS
         
         
@@ -38,7 +45,69 @@ class Subscription(models.Model):
             self.stripe_id = stripe_id
             super().save(*args, **kwargs)
                      
+
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe Price
+    """  
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly" 
+        YEARLY = "year", "Yearly"
         
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)  
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)  
+    interval = models.CharField(max_length=120, 
+                                default= IntervalChoices.MONTHLY, 
+                                choices=IntervalChoices.choices)
+    price = models.DecimalField(max_digits=10,  decimal_places=2, default=99.99)
+    order = models.IntegerField(default=-1, help_text='Ordering on Django pricing page')
+    featured = models.BooleanField(default=True, help_text='Featured on Django pricing page')
+    updated = models.DateTimeField(auto_now=True)
+    Timestamp = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['subscription__order', 'order', 'featured', '-updated']
+    
+    @property
+    def stripe_currency(self):
+        return "usd"
+    
+    @property
+    def stripe_price(self):
+        
+        """
+        remove deciaml places
+        """
+        return int(self.price * 100)
+    
+    
+    @property
+    def product_stripe_id(self):   
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id 
+    
+    
+    def save(self, *args, **kwargs):
+        if (not self.stripe_id and self.product_stripe_id is not None):
+            stripe_id = helpers.billing.create_price(
+                currency=self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,
+                product=self.product_stripe_id,
+                metadata={
+                    "subscription_plan_price_id": self.id},
+                raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+        if self.featured and self.subscription:
+            qs = SubscriptionPrice.objects.filter(
+                subscription=self.subscription,
+                interval=self.interval,
+            ).exclude(id=self.id)
+            qs.update(featured=False)
+       
         
 class UserSubscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
